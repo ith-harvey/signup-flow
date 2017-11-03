bf0e52e6cd45a60f6d453074cee28c6caaf1425a
Ian Harvey <ith.harvey@gmail.com>
2017-11-02 23:07:23 -0500

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
+// Base Imports
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
+
+// Materialize
+import { MaterializeModule } from "angular2-materialize";
+
+// Components
+import { SubscriptionComponent } from './subscription/subscription.component';
+import { ConfirmComponent } from './confirm/confirm.component';
+import { UserInputComponent } from './user-input/user-input.component';
+import { UserDataService} from './user-data.service'
+
+
+
+const appRoutes: Routes = [
+  {path: 'signup', component: UserInputComponent },
+  {path: 'subscription', component: SubscriptionComponent },
+  {path: 'confirm', component: ConfirmComponent },
+  {path: '', component: UserInputComponent }
+]
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+  declarations: [AppComponent, SubscriptionComponent, ConfirmComponent, UserInputComponent],
+  imports: [BrowserModule, RouterModule.forRoot(appRoutes), MaterializeModule],
+  providers: [UserDataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
