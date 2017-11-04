c35f8a4d962d2a3819fa9d208db27f4f0c242dfa
Ian Harvey <ith.harvey@gmail.com>
2017-11-04 15:13:57 -0500

    added coors

--- server.js
+++ server.js
@@ -3,6 +3,15 @@ const app = express();
 
 // Run the app by serving the static files
 // in the dist directory
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", 'https://mysterious-beyond-57369.herokuapp.com/'); //<-- you can change this with a specific url like http://localhost:4200
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
+
+
 app.use(express.static(__dirname + '/dist'));
 // Start the app by listening on the default
 // Heroku port
